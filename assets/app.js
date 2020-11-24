da3bb8299d15cc731e1b91d57af77d2d298b7df8
Thomas Leroy <thomasleroy50@gmail.com>
2020-11-24 12:26:52 +0100

    quete_6 Symfony Ok

--- assets/app.js
+++ assets/app.js
@@ -7,8 +7,15 @@
 
 // any CSS you import will output into a single css file (app.scss in this case)
 import './styles/app.scss';
+import "node_modules/bootstrap/scss/bootstrap";
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
+import $ from 'jquery';
+const $ = require('jquery');
+
+require('bootstrap');
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
 
 console.log('Hello Webpack Encore! Edit me in assets/app.js');
